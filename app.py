503df536087c8a905adc5c1e5f45467fcc229668
Xeonym82 <xeonym82@gmail.com>
2025-12-15 16:23:05 +0100

    Fixed syntax error

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ with tab2:
                 
                 if cus_id:
                     if is_duplicate:
-                        st.warning(f⚠️ Account exists! Using existing ID: {cus_id}")
+                        st.warning(f"⚠️ Account exists! Using existing ID: {cus_id}")
                     else:
                         st.success(f"✅ New Customer created: {cus_id}")
                     
